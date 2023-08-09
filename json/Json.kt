572fb69a20b1fb478dc66eeee96d55446e4e717b
Dima Korolev <dmitry.korolev+github-2022@gmail.com>
2023-08-10 03:24:53 +0400

    Constructing a JSON object too.

--- json/Json.kt
+++ json/Json.kt
@@ -3,4 +3,7 @@ import org.json.JSONObject
 fun main() {
     val obj = JSONObject("""{"foo":"bar"}""")
     println("""The value for the key "foo" is "${obj.getString("foo")}".""")
+    val res = JSONObject()
+    res.put("exFoo", obj.getString("foo"))
+    println("""And here's the newly constructed object: ${res.toString()}.""")
 }
